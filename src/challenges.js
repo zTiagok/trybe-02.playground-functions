6e51b09f0de702bc711928d182c9c41c40e4228e
Tiago Braga Costa <ztiagok@gmail.com>
2022-04-12 14:46:58 -0300

    Resolução do desafio 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,42 +1,68 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(aItem, bItem) {
   // seu código aqui
+
+  if (aItem === true && bItem === true)
+  {
+
+    return true;
+  }
+  
+    return false;
 }
 
+// -------------------------------------------------
+
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+
+  return (base * height) / 2;
 }
+
+// -------------------------------------------------
 
 // Desafio 3
 function splitSentence() {
   // seu código aqui
 }
 
+// -------------------------------------------------
+
 // Desafio 4
 function concatName() {
   // seu código aqui
 }
+
+// -------------------------------------------------
 
 // Desafio 5
 function footballPoints() {
   // seu código aqui
 }
 
+// -------------------------------------------------
+
 // Desafio 6
 function highestCount() {
   // seu código aqui
 }
+
+// -------------------------------------------------
 
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
 }
 
+// -------------------------------------------------
+
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
 }
+
+// -------------------------------------------------
 
 // Desafio 9
 function encode() {
@@ -46,10 +72,14 @@ function decode() {
   // seu código aqui
 }
 
+// -------------------------------------------------
+
 // Desafio 10
 function techList() {
   // seu código aqui
 }
+
+// -------------------------------------------------
 
 module.exports = {
   calcArea,
